06803f762c4f169fa10f7d1c0925197dbe9c4a0e
Raymond <kokyuhang2004@gmail.com>
2025-04-19 16:14:30 +0800

    add tab navigator

--- App.tsx
+++ App.tsx
@@ -1,16 +1,35 @@
 import React from 'react'
+import {NavigationContainer} from '@react-navigation/native'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import sedanList from './carList_tab/sedanList';
+import suvList from './carList_tab/suvList';
+import luxuryList from './carList_tab/luxuryList';
+import { LogBox } from 'react-native';
 import {Text, View} from 'react-native';
 
+LogBox.ignoreLogs(['EventEmitter.removeListener']);
+
+const Tab = createBottomTabNavigator();
+
 const App = () => {
-    return (
-        <View>
-            <Text>Hello world</Text>
-            <Text>Hello world</Text>
-            <Text>Hello world</Text>
-            <Text>Hello world</Text>
-            <Text>Hello world</Text>
-        </View>
-    );
+    return(
+        <NavigationContainer>
+            <Tab.Navigator initialRouteName='sedanList'>
+                <Tab.Screen
+                    name='suvList'
+                    component={suvList}
+                />
+                <Tab.Screen
+                    name='sedanList'
+                    component={sedanList}
+                />
+                <Tab.Screen
+                    name='luxuryList'
+                    component={luxuryList}
+                />
+            </Tab.Navigator>
+        </NavigationContainer>
+    )
 }
 
 export default App;
---
+++ carList_tab/suvList.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Text, View} from 'react-native';
+
+const App = () => {
+    return (
+        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+            <Text style={{fontSize: 50, color: 'black'}}>SUV Car List</Text>
+        </View>
+    );
+}
+
+export default App;
