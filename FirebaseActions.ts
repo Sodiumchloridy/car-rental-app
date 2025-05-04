1ae400ef8d808540a4948589e13debd625d1fe92
Raymond <kokyuhang2004@gmail.com>
2025-05-04 16:23:17 +0800

    finish flow: home -> carlist -> booking page -> booking confirm page -> home
    - added new firebaseActions
    - added file TimeFormating.ts
    - adjust Types.ts for Booking type
    - modify UI.ts for displayWithLabel
    - added async and await for uploading booking info and retrive booking

--- FirebaseActions.ts
+++ FirebaseActions.ts
@@ -1,25 +1,30 @@
 import { initializeApp, getApp } from 'firebase/app';
 import { getFirestore, collection, addDoc, query, where, orderBy, limit, getDoc, getDocs, doc, updateDoc } from '@react-native-firebase/firestore';
-import { Booking } from './Types';
+import { Booking, Car } from './Types';
+import { formatDateTime, parseDateTime } from './TimeFormating';
 
 const firestoreDB = getFirestore();
 const bookingsCollection = collection(firestoreDB, 'bookings');
 const carsCollection = collection(firestoreDB, 'cars');
+
+
 
 // this function is to upload user booking
 export const uploadBooking = async (bookingData: Booking) => {
     try {
         const bookingRef = await addDoc(bookingsCollection, {
             ...bookingData,
-            booking_date: bookingData.booking_date.toISOString(),
-            start_date: bookingData.start_date.toISOString(),
-            end_date: bookingData.end_date.toISOString(),
+            booking_date: bookingData.booking_date,
+            start_date: bookingData.start_date,
+            end_date: bookingData.end_date,
         });
         console.log(bookingData.payment);
         console.log('Booking uploaded successfully, ID:', bookingRef.id);
         changeAvailability(bookingData.car_id, bookingData.start_date, bookingData.end_date, false);
+        return true;
     } catch (error) {
         console.error('Error uploading booking:', error);
+        return false;
     }
 };
 
@@ -36,8 +41,9 @@ export const getLatestBooking = async (userID: string) => {
 
     if (!latestBooking.empty) {
         const doc = latestBooking.docs[0];
-        console.log("Recent Booking: ", doc.id, doc.data());
-        return { booking_id: doc.id, ...doc.data() };
+        const data = doc.data() as Booking;
+        console.log("Recent Booking: ", doc.id, data);
+        return { booking_id: doc.id, bookingData: data };
     } else {
         console.log('No latest booking found');
         return null;
@@ -45,7 +51,7 @@ export const getLatestBooking = async (userID: string) => {
 }
 
 // a function that adjust the availability of a car after a booking is placed
-export const changeAvailability = async (carID: string, start_date: Date, end_date: Date, isAvailable: boolean) => {
+export const changeAvailability = async (carID: string, start_date: string, end_date: string, isAvailable: boolean) => {
     try {
         // search car based on carID
         const carSelected = doc(carsCollection, carID);
@@ -53,13 +59,13 @@ export const changeAvailability = async (carID: string, start_date: Date, end_da
         const carData = await getDoc(carSelected);
 
         if (carData.exists) {
-            const available_date = new Date(end_date);
+            const available_date = parseDateTime(end_date);
             available_date.setDate(available_date.getDate() + 1);
-            available_date.setUTCHours(0, 0, 0, 0);
+            available_date.setHours(0, 0, 0, 0);
             await updateDoc(carSelected, {
                 availability: isAvailable ? 'yes' : 'booked',
-                unavailable_from: isAvailable ? null : start_date.toISOString(),
-                unavailable_until: isAvailable ? null : available_date.toISOString(),
+                unavailable_from: isAvailable ? null : start_date,
+                unavailable_until: isAvailable ? null : formatDateTime(available_date),
             })
             console.log(`Car availability updated: ${carID}, Availability: ${isAvailable}`)
         } else {
@@ -68,5 +74,22 @@ export const changeAvailability = async (carID: string, start_date: Date, end_da
 
     } catch (error) {
         console.error('Error updating car availability:', error);
+    }
+}
+
+// retrieve a car based on car id provided
+export const getCar = async (carID: string) => {
+    try {
+        const carSelected = doc(carsCollection, carID);
+        const carData = await getDoc(carSelected);
+        if (carData.exists) {
+            const car = carData.data() as Car;
+            console.log("Car id: " + carID + " is found");
+            return car;
+        } else {
+            console.log("Car not found");
+        }
+    } catch (error) {
+        console.log("Error when retrieving car: " + error)
     }
 }
